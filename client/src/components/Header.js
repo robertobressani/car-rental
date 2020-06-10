ca7ff7cab33af9d89954e6a7bdab5f2dd51916f9
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-10 16:55:09 +0200

    feat(src): add page navigation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,49 +1,55 @@
-import React from "react";
+import React, {useContext} from "react";
 import {Navbar, Nav, Col, Image, Button} from "react-bootstrap";
-import { Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link, NavLink} from 'react-router-dom';
 import Logo from "./img/logo.svg"
-
+import AuthenticationContext from "./AuthenticationContext.js";
 
 
 function Header(props) {
+    let button;
+    const value = useContext(AuthenticationContext);
+    if (value.loggedIn)
+        button = <Link to="/cars"><Button variant="danger" onClick={value.logout}>{"Logout"} </Button></Link>;
+    else
+        button = <Link to="/login"><Button variant="success">{"Login"} </Button></Link>;
     return <>
         <Navbar bg="dark" variant="dark" className="row justify-content-between">
-                <Col md={1} sm={4}>
-                    <Image src={Logo} height={50} rounded />
-                </Col>
-                <Col md={3}>
-                    <Navbar.Brand><b className="d-none d-sm-inline">CAR RENTAL</b></Navbar.Brand>
-                </Col>
-                <Col md={2} sm={4}>
-                    <Switch>
-                        <Route exact path="/login" />
-                        <Route path="/">
-                            <Button variant={props.loggedIn? "danger" : "success"}>{props.loggedIn? "Logout" : "Login"}</Button>
-                        </Route>
-                    </Switch>
+            <Col md={1} sm={4}>
+                <Image src={Logo} height={50} rounded/>
+            </Col>
+            <Col md={4}>
+                <Navbar.Brand><b className="d-none d-sm-inline">CAR RENTAL</b></Navbar.Brand>
+            </Col>
+            {value.loggedIn ?
+                <Col md={3}
+                     className="d-none d-md-inline"><Navbar.Text>Hello, {value.userName}!</Navbar.Text></Col> : <></>}
+            <Col md={2} sm={4}>
+                <Switch>
+                    <Route exact path="/login"/>
+                    <Route path="/">
+                        {button}
+                    </Route>
+                </Switch>
 
-                </Col>
+            </Col>
         </Navbar>
-        <NavigationPanel show={props.loggedIn}/>
+        <NavigationPanel show={value.loggedIn}/>
     </>;
 }
 
-function NavigationPanel(props){
-    if(props.show)
+function NavigationPanel(props) {
+    if (props.show)
         //user is logged in, navigation panel should be shown
         //TODO change bg color
-        //TODO change links
-        return <Nav bg="dark" variant="tabs" defaultActiveKey="/home">
+        return <Nav variant="tabs" >
             <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
+                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-light" to="/cars">All cars  </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-light" to="/configurator">Rental configurator  </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                </Nav.Link>
+                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-light" to="/rentals">List of all rentals </Nav.Link>
             </Nav.Item>
         </Nav>;
     //nothing to show if the user is not authenticated
