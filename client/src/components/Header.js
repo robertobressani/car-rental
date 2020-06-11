34b2846da586f2177ef17051573d32eb6dd3b25f
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-11 19:26:03 +0200

    feat(src): added mobile navbar version

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import Logo from "./img/logo.svg"
 import AuthenticationContext from "./AuthenticationContext.js";
 
 
-function Header(props) {
+function Header() {
     let button;
     const value = useContext(AuthenticationContext);
     if(!value.verifiedLogin)
@@ -24,45 +24,56 @@ function Header(props) {
     else
         button = <Link to="/login"><Button variant="success">{"Login"} </Button></Link>;
     return <>
-        <Navbar bg="dark" variant="dark" className="row justify-content-between">
-            <Col md={1} sm={4}>
+        <Navbar  collapseOnSelect bg="dark" expand="md" variant="dark" className="row justify-content-between">
+            <Col md={0} xs={1} className=" d-md-none">
+                <Navbar.Toggle   aria-controls="nav-panel" />
+            </Col>
+            <Col xs={1}>
                 <Image src={Logo} height={50} rounded/>
             </Col>
-            <Col md={4}>
-                <Navbar.Brand><b className="d-none d-sm-inline">CAR RENTAL</b></Navbar.Brand>
+            <Col md={4} className="d-none d-md-inline">
+                <Navbar.Brand><b >CAR RENTAL</b></Navbar.Brand>
             </Col>
             {value.loggedIn ?
                 <Col md={3}
                      className="d-none d-md-inline"><Navbar.Text>Hello, {value.userName}!</Navbar.Text></Col> : <></>}
-            <Col md={2} sm={4}>
+            <Col xs={4}>
                 <Switch>
-                    <Route exact path="/login"/>
+                    <Route exact path="/login"/> {/*login/logout button not shown in login page*/}
                     <Route path="/">
                         {button}
                     </Route>
                 </Switch>
 
             </Col>
+            <NavigationPanel show={value.loggedIn} mobile={true}/>
         </Navbar>
-        <NavigationPanel show={value.loggedIn}/>
+        <NavigationPanel show={value.loggedIn} mobile={false}/>
+
     </>;
 }
 
 function NavigationPanel(props) {
-    if (props.show)
+    const content=<><Nav.Item> {/*href is put to make collapseOnSelect functionalities working*/}
+        <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" href="/cars" to="/cars">All cars  </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" href="/configurator" to="/configurator">Rental configurator  </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+        <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" href="/rentals" to="/rentals">List of all rentals </Nav.Link>
+    </Nav.Item></>;
+    if (props.show && !props.mobile)
         //user is logged in, navigation panel should be shown
-        //TODO change bg color
-        return <Nav variant="tabs" >
-            <Nav.Item>
-                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" to="/cars">All cars  </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" to="/configurator">Rental configurator  </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={NavLink}  activeClassName="bg-white" className="bg-grey text-dark" to="/rentals">List of all rentals </Nav.Link>
-            </Nav.Item>
+        return <Nav variant="tabs"  className="d-none d-md-flex ">
+            {content}
         </Nav>;
+    else if(props.show && props.mobile)
+        return <Navbar.Collapse  >
+            <Nav className={"d-inline d-md-none col-12"} id="nav-panel">
+                {content}
+            </Nav>
+        </Navbar.Collapse>;
     //nothing to show if the user is not authenticated
     return <></>;
 }
