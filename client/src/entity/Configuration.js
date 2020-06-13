5efc47ddcfe0a1f876d89f11ee233f43e786b71d
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-13 21:34:20 +0200

    feat(src): added configuration entity class

---
+++ client/src/entity/Configuration.js
@@ -0,0 +1,27 @@
+import moment from "moment";
+import {dateDiff} from "../utils/dateDiff";
+
+export default  class Configuration{
+    constructor(start="", end="", category="", kilometer=0, unlimited=false, age=18, extra_drivers=0, insurance=false) {
+        this.start=moment(start);
+        this.end=moment(end);
+        this.category= category;
+        this.kilometer=kilometer;
+        this.unlimited=unlimited;
+        this.age=age;
+        this.extra_drivers=extra_drivers;
+        this.insurance=insurance;
+    }
+
+    isValid(){
+        let today=moment();
+        today.hour(0);
+        today.minute(0);
+        today.second(0)
+        today.millisecond(0);
+        return this.start && dateDiff(this.start, today) > 0
+            && this.end && dateDiff(this.end, this.start) > 0 && ["A", "B", "C", "D", "E", "F"].includes(this.category)
+            && (this.unlimited || this.kilometer > 0) && this.age >= 18 && +this.extra_drivers >= 0; //check whether it's integer
+
+    }
+}
