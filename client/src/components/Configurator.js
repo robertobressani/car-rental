225aa9202c806ecf3a25ea549a5c9030b0f038c0
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-14 19:56:58 +0200

    feat(src): added PaymentDialog

--- client/src/components/Configurator.js
+++ client/src/components/Configurator.js
@@ -1,37 +1,36 @@
-import React, {useContext} from 'react';
-import {Redirect} from 'react-router-dom';
-import {Form, Jumbotron, ProgressBar, Row} from 'react-bootstrap';
+import React from 'react';
+import {Redirect, Route, Link} from 'react-router-dom';
+import {Form, Jumbotron, ProgressBar, Row, Alert, Button, Modal} from 'react-bootstrap';
 import AuthenticationContext from './AuthenticationContext.js';
 
 import moment from 'moment';
 import Configuration from '../entity/Configuration.js';
 
-function Configurator() {
-    const value = useContext(AuthenticationContext);
-    if (!value.verifiedLogin)
-        return <Jumbotron className="jumbotron-space"><ProgressBar animated now={100}/></Jumbotron>;
-    if (!value.loggedIn)
-        return <Redirect to={"/login"}/>
-    return <><Jumbotron className=" jumbotron-space">
-        <ConfiguratorForm/>
-    </Jumbotron></>;
-    //TODO add component for number of car available
-}
-
-class ConfiguratorForm extends React.Component {
+class Configurator extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {configuration: new Configuration()}
+        this.state={submitted: false, loading:false, configuration: new Configuration(), price_num:{ price:-1, available:-1}}
     }
 
+    /**
+     * form has changed: must load the price and availability
+     */
     componentDidUpdate() {
-        if (this.state.configuration.isValid()) {
+        //loading of new data must be perfomed only when a sumbission hasn't been already done
+        if (!this.state.submitted && this.state.configuration.isValid()) {
             console.log("that's the right time to update");
+
+            //TODO loading must be set to true (debug to false)
+            this.setState({submitted: true, loading:false});
             let configuration = Object.assign(new Configuration(), {...this.state.configuration});
             if (configuration.unlimited)
                 //make no sense have kilometers per day set to a number
                 configuration.kilometer=0;
-            //TODO add API call
+            //TODO add API call then
+            //TODO add validation messages
+            //TODO remove this call (debug)
+            this.setState({price_num:{price: 1000, available: 2}})
+            //tthis.setState({ loading:false});
         }
     }
 
@@ -39,28 +38,54 @@ class ConfiguratorForm extends React.Component {
         this.setState((state) => {
             let configuration = Object.assign(new Configuration(), {...state.configuration});
             configuration[name] = value;
-            console.log(name + " " + value);
-            return {configuration};
+            //setting submitted= false, so that a new load can be performed if valid
+            return {configuration: configuration, submitted:false};
         });
     }
 
     render() {
+        return <AuthenticationContext.Consumer >{(value) =>{
+            if (!value.verifiedLogin)
+                return <Jumbotron className="jumbotron-space"><ProgressBar animated now={100}/></Jumbotron>;
+            if (!value.loggedIn)
+                return <Redirect to={"/login"}/>
+            return <><Jumbotron className=" jumbotron-space">
+                <ConfiguratorForm  updateValue={(name, value)=>this.updateValue(name, value)} configuration={this.state.configuration}/>
+                </Jumbotron>
+                {this.state.submitted ?
+                    <AvailableCar loading={this.state.loading} price_num={this.state.price_num}/> : <></>
+                }
+                <Route exact path="/configurator/pay">
+                <PaymentDialog configuration={this.state.configuration}/>
+                </Route>
+            </>;
+
+            }
+
+        }</AuthenticationContext.Consumer>;
+
+
+    }
+}
+
+function ConfiguratorForm(props){
         return <Form className="row ">
             <Form.Group className="col-12 col-md-4">
                 <Form.Label>Start date of rental:</Form.Label>
-                <Form.Control type="date" defaultValue={this.state.configuration.start}
-                              onChange={(event) => this.updateValue("start", moment(event.target.value))}/>
+                <Form.Control type="date" defaultValue={props.configuration.start ?
+                                    props.configuration.start.format("yyyy-MM-DD"): ""}
+                              onChange={(event) => props.updateValue("start", moment(event.target.value))}/>
             </Form.Group>
-
             <Form.Group className="col-12 col-md-4">
                 <Form.Label>End date of rental:</Form.Label>
-                <Form.Control type="date" defaultValue={this.state.configuration.end}
-                              onChange={(event) => this.updateValue("end", moment(event.target.value))}/>
+                <Form.Control type="date" defaultValue={props.configuration.end ?
+                                    props.configuration.end.format("yyyy-MM-DD"): ""}
+                              onChange={(event) => props.updateValue("end", moment(event.target.value))}/>
             </Form.Group>
             <Form.Group className="col-12 col-md-4">
                 <Form.Label> Category: </Form.Label>
-                <Form.Control as="select" defaultValue={this.state.configuration.category}
-                              onChange={(event) => this.updateValue("category", event.target.value)}>
+                <Form.Control as="select" defaultValue={props.configuration.category}
+                              onChange={(event) => props.updateValue("category", event.target.value)}>
                     <option/>
                     <option>A</option>
                     <option>B</option>
@@ -72,33 +97,104 @@ class ConfiguratorForm extends React.Component {
             </Form.Group>
             <Form.Group className="col-6 col-md-2">
                 <Form.Label>Age of driver: </Form.Label>
-                <Form.Control type="number" defaultValue={this.state.configuration.age}
-                              onChange={(event) => this.updateValue("age", event.target.value)}
+                <Form.Control type="number" defaultValue={props.configuration.age}
+                              onChange={(event) => props.updateValue("age", event.target.value)}
                               min={18}/>
             </Form.Group>
             <Form.Group className="col-6 col-md-2">
                 <Form.Label> Extra drivers: </Form.Label>
-                <Form.Control type="number" defaultValue={this.state.configuration.extra_drivers}
-                              onChange={(event) => this.updateValue("extra_drivers", event.target.value)}/>
+                <Form.Control type="number" defaultValue={props.configuration.extra_drivers}
+                              onChange={(event) => props.updateValue("extra_drivers", event.target.value)}/>
             </Form.Group>
             <Form.Group className="col-12 col-md-4">
                 <Row className="justify-content-around">
-                    <Form.Label>Kilometers per day : {this.state.configuration.unlimited ?
-                        "limitless" : this.state.configuration.kilometer}</Form.Label>
-                    <Form.Check className="col-4" label="Unlimited" defaultChecked={this.state.configuration.unlimited}
-                                onChange={(event) => this.updateValue("unlimited", event.target.checked)}/>
+                    <Form.Label>Kilometers per day : {props.configuration.unlimited ?
+                        "limitless" : props.configuration.kilometer}</Form.Label>
+                    <Form.Check className="col-4" label="Unlimited" defaultChecked={props.configuration.unlimited}
+                                onChange={(event) => props.updateValue("unlimited", event.target.checked)}/>
                 </Row>
-                <Form.Control type="range" min={0} max={1000} value={this.state.configuration.kilometer}
-                              onChange={(event) => this.updateValue("kilometer", event.target.value)}
-                              disabled={this.state.configuration.unlimited}/>
+                <Form.Control type="range" min={0} max={1000} value={props.configuration.kilometer}
+                              onChange={(event) => props.updateValue("kilometer", event.target.value)}
+                              disabled={props.configuration.unlimited}/>
             </Form.Group>
             <Form.Group className="col-12 col-md-3">
-                <Form.Check defaultChecked={this.state.configuration.insurance}
-                            onChange={(event) => this.updateValue("insurance", event.target.checked)}
+                <Form.Check defaultChecked={props.configuration.insurance}
+                            onChange={(event) => props.updateValue("insurance", event.target.checked)}
                             label="Extra insurance"/>
             </Form.Group>
         </Form>
-    }
+}
+
+function AvailableCar(props){
+    if(props.loading)
+        return <ProgressBar animated now={100}/>
+    else if(props.price_num.available> 0)
+        return <Alert variant="success">
+            <Alert.Heading>We've found a car for you!</Alert.Heading>
+            <p>
+                There are {props.price_num.available} car{props.price_num.available>1 ? "s" :""} that satisf
+                {props.price_num.available>1 ? "y" :"ies"} your search available at {props.price_num.price} €
+            </p>
+            <hr />
+            <div className="d-flex justify-content-end">
+                <Link to="/configurator/pay"><Button variant="outline-success">
+                    Proceed to payment
+                </Button></Link>
+            </div>
+        </Alert>;
+    else return  <Alert variant="danger">
+        <Alert.Heading>There are no cars for you!</Alert.Heading>
+        <p>
+            We are sorry, we cannot rent any car according to the search you have perfomed.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+            <Button variant="outline-danger">
+                Notify me when a car is available
+            </Button>
+        </div>
+    </Alert>;
+}
+
+function PaymentDialog(props) {
+    if(!props.configuration.isValid())
+        return <Redirect to={"/configurator"}/>;
+    console.log("modal");
+    return <Modal show={true}>
+        <Modal.Header closeButton >
+            <Modal.Title>Insert your payment data</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <PaymentForm/>
+        </Modal.Body>
+
+        <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+    </Modal>;
+}
+
+function PaymentForm(props){
+    return <Form>
+        <Form.Group>
+            <Form.Label>Credit card owner (full name):</Form.Label>
+            <Form.Control type="text" required/>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label> Card number</Form.Label>
+            <Form.Control type="text"  placeholder="Your card number"  required>
+                    <div className="input-group-append">
+                    <span className="input-group-text text-muted">
+                                                <i className="fa fa-cc-visa mx-1"></i>
+                                                <i className="fa fa-cc-amex mx-1"></i>
+                                                <i className="fa fa-cc-mastercard mx-1"></i>
+                                            </span>
+                    </div>
+            </Form.Control>
+        </Form.Group>
+    </Form>;
 }
 
 export default Configurator;
