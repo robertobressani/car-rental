abe8bec49b8159f464eb27cefd4bfe539e8e9f4d
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-16 19:49:38 +0200

    feat(src): added morgan dependency

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
-const PORT = 3001;
+const serverConf =require('./config/server_conf');
+
+const PORT = serverConf.Port;
 
 app = new express();
+app.use(morgan('tiny'));
+
+
+/**-----PUBLIC APIs---------*/
+
+
+/**----FROM NOW ON ONLY PRIVATE API ----- */
+
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
