50965d3ac38f20f9662a237cffbf4f1414045bab
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-17 18:25:51 +0200

    feat(src): implemented some API calls

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,6 +1,27 @@
 import Car from "../entity/Car";
 import Rental from "../entity/Rental";
 
+const BASE_URL="/api/";
+
+async function getCars(){
+    const response = await fetch(`${BASE_URL}cars`);
+    const jsonCars = await response.json();
+    if(!response.ok)
+        throw {status: response.status, message: jsonCars.msg }
+    return  jsonCars.map(json=>Car.of(json));
+    //return [new Car(1,"Alfa", "A","Romeo", 100 ), new Car(2, "Fiat", "E", "500",500)];
+}
+
+async function getBrands(){
+    const response = await fetch(`${BASE_URL}brands`);
+    const jsonBrands = await response.json();
+    if(!response.ok)
+        throw {status: response.status, message: jsonBrands.msg }
+    return  jsonBrands;
+
+    //return ["Alfa", "Fiat"];
+}
+
 async function checkAuthentication() {
     //TODO implement
     return new Promise(((resolve, reject) =>
@@ -17,18 +38,11 @@ async function login(email, password){
     //return true;
 }
 
+
+}
+
 async function logout(){
     //TODO implement
-}
-
-async function getCars(){
-    //TODO implement
-    return [new Car(1,"Alfa", "A","Romeo", 100 ), new Car(2, "Fiat", "E", "500",500)];
-}
-
-async function getBrands(){
-    //TODO implement
-    return ["Alfa", "Fiat"];
 }
 
 async function getRentals(future){
