0e8e48c186943fed235e704a7441a0bf36bc22cf
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-17 18:26:29 +0200

    feat(src): implemented some REST API

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const carDao= require('./dao/car_dao')
 
 const serverConf =require('./config/server_conf');
 
 const PORT = serverConf.port;
+const BASE_URL=serverConf.baseURL;
 
 app = new express();
 app.use(morgan('tiny'));
@@ -11,6 +13,33 @@ app.use(morgan('tiny'));
 
 /**-----PUBLIC APIs---------*/
 
+/**
+ * get the list of cars
+ * @param NONE
+ * @return the list of all cars
+ */
+app.get(BASE_URL+"cars",(req, res)=>{
+
+    carDao.getCar().then(cars=>res.json(cars)).catch((err) => {
+        res.status(500).json({
+            errors: [{ msg: err}],
+        });
+    });
+});
+
+/**
+ * get the list of brands
+ * @param NONE
+ * @return the list of all brands
+ */
+app.get(BASE_URL+"brands",(req, res)=>{
+
+    carDao.getBrands().then(cars=>res.json(cars)).catch((err) => {
+        res.status(500).json({
+            errors: [{ msg: err}],
+        });
+    });
+});
 
 /**----FROM NOW ON ONLY PRIVATE API ----- */
 
