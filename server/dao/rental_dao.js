6cd51e56052458ff175cfafd0154c5a0400b70cd
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-18 19:40:18 +0200

    feat(src): added userId param to searchRental

--- server/dao/rental_dao.js
+++ server/dao/rental_dao.js
@@ -1,7 +1,7 @@
 const Configuration= require('../entity/Configuration');
 const db = require('../db/db');
 
-module.exports.searchRental=async (conf)=>{
+module.exports.searchRental=async (conf, userId)=>{
 
     return {price: 1000, available: 2};
 }
