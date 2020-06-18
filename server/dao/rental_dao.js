e4cdb10ea09a3aa854612ef811d98c92e13cb64a
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-18 19:05:30 +0200

    feat(src): added rental_dao file

---
+++ server/dao/rental_dao.js
@@ -0,0 +1,7 @@
+const Configuration= require('../entity/Configuration');
+const db = require('../db/db');
+
+module.exports.searchRental=async (conf)=>{
+
+    return {price: 1000, available: 2};
+}
