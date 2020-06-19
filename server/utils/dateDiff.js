807262d727ea2e718c8622aaf0af26420d1165be
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-19 19:17:54 +0200

    feat(src): renamed dateUtils module

--- server/utils/dateDiff.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment');
-
-module.exports = (end, start)=>{
-    return moment.duration(end.diff(start)).asDays();
-}
---
+++ server/utils/dateUtils.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+module.exports.dateDiff = (end, start)=>{
+    return moment.duration(end.diff(start)).asDays();
+    return moment.duration(end.diff(start)).asDays();
+}
+
+module.exports.dateFormat = (date)=>{
+    return date.format("yyyy-MM-DD");
+}
