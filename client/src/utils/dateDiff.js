3e0b6787550bc848aa6cc639fd6a6e776cdbab37
Roberto Bressani <roberto.bressani97@gmail.com>
2020-06-22 19:45:43 +0200

    feat(src): implemented dateFormat client-side

--- client/src/utils/dateDiff.js
+++ client/src/utils/dateDiff.js
@@ -3,3 +3,7 @@ import moment from 'moment';
 export  function dateDiff(end, start){
     return moment.duration(end.diff(start)).asDays();
 }
+
+export function dateFormat(date){
+    return date.format("yyyy-MM-DD");
+}
